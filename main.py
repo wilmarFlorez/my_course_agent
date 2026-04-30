6711571c3e26b34f02c5373cfabd40dd439723a2
Wilmar F.S. <wflorez52@gmail.com>
2026-04-30 09:49:05 -0500

    init langgraph

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from my-course-agent!")
+# pip install -qU langchain "langchain[openai]"
+from langchain.agents import create_agent
 
 
-if __name__ == "__main__":
-    main()
+def get_weather(city: str) -> str:
+    """Get weather for a given city."""
+    return f"It's always sunny in {city}!"
+
+
+agent = create_agent(
+    model="openai:gpt-5.4-mini",
+    tools=[get_weather],
+    system_prompt="You are a helpful assistant",
+)
+
+
