131387f70d34a445bbdacd3e8bb1a2994629db92
Wilmar F.S. <wflorez52@gmail.com>
2026-05-01 15:47:07 -0500

    Add simple state management implementation with LLM interaction

---
+++ src/agents/simple.py
@@ -0,0 +1,34 @@
+from random import randint
+
+from langchain.chat_models import init_chat_model
+from langgraph.graph import END, START, MessagesState, StateGraph
+
+llm = init_chat_model("gpt-5.4-nano", temperature=1)
+
+
+class State(MessagesState):
+    customer_name: str
+    my_age: int
+
+
+def node_1(state: State):
+    new_state: State = {}
+    if state.get("customer_name") is None:
+        new_state["customer_name"] = "Wilmar Florez"
+    else:
+        new_state["my_age"] = randint(18, 90)
+
+    history = state["messages"]
+    ai_message = llm.invoke(history)
+    new_state["messages"] = [ai_message]
+    print(new_state)
+    return new_state
+
+
+builder = StateGraph(State)
+builder.add_node("node_1", node_1)
+
+builder.add_edge(START, "node_1")
+builder.add_edge("node_1", END)
+
+agent = builder.compile()
